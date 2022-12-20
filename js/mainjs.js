f18c21911a8edfe6c878e28e042d8ba6e2a21493
githubt-hyakutake <github116937732+t-hyakutake@users.noreply.github.com>
2022-12-20 13:54:54 +0900

    ナビ

---
+++ js/mainjs.js
@@ -0,0 +1,12 @@
+'use strict'
+{
+let navi = document.querySelector('.nav')
+console.log(navi)
+let navHeight = navi.getBoundingClientRect().height;
+console.log(navHeight)
+
+let footer = document.querySelector('footer');
+console.log(footer) 
+
+// footer.style.paddingBottom = '${navHeight}px'
+}
